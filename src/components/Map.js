47c07be3d06ace80b8b88f703924c279d7153f90
Prakash <prkjha25@gmail.com>
2021-01-22 00:51:03 +0530

    map view

---
+++ src/components/Map.js
@@ -0,0 +1,26 @@
+import GoogleMapReact from 'google-map-react'
+
+const Map = ({ center, zoom }) => {
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
+}
+
+
+export default Map
