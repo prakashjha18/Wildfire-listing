def50b4f524ee01483cc1609e3e1472f6690858c
prakash <prkjha25@gmail.com>
2021-01-22 02:55:53 +0530

    wildfire listing

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,18 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
-const Map = ({ center, zoom }) => {
+const Map = ({ eventData,center, zoom }) => {
+
+    const [locationInfo, setLocationInfo] = useState(null)
+
+    const markers = eventData.map((ev, index) => {
+        if(ev.categories[0].id === 8) {
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}  onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>
+        }
+        return null
+    })
     return (
         <div className="map">
             <GoogleMapReact
@@ -8,8 +20,10 @@ const Map = ({ center, zoom }) => {
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
-
+            {markers}
             </GoogleMapReact>
+
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
